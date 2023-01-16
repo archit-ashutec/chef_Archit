27044b55767dac8ceeaaed2f5501888e580b1447
archit-ashutec <archit@ashutec.com>
2023-01-16 11:42:12 +0530

    gitignore added

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,25 +1,7 @@
 import View from './view.js';
 
 import icons from 'url:../../img/icons.svg';
-
-const Fraction = count => {
-  if (count) {
-    const [int, dec] = count
-      .toString()
-      .split('.')
-      .map(e => parseInt(e));
-    if (!dec) return count;
-    if (int === 0) {
-      const fr = new fraction(count);
-      return console.log(`${fr.numerator}/${fr.denominator}`);
-    }
-    if (int > 0) {
-      const fr = new fraction(count - int);
-      return `${int} ${fr.numerator}/${fr.denominator}`;
-    }
-  }
-  return '?';
-};
+import { Fraction } from 'fractional';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
